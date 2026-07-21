04756c7318c4a546c77f2ca31dbe29de6182cc74
AngiesData <data@avidityexchange.com>
2026-07-20 22:36:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const resumeData = {
   name: "Angela",
 
   headline:
-    "Lead Financial Clearance | Revenue Cycle | Healthcare Operations | CIS | Business Administration",
+    "Lead Financial Clearance | Revenue Cycle | IP | Surgical | Maternity",
 
   summary:
     "Adaptable healthcare operations and information technology professional with experience in financial clearance, revenue cycle processing, employee training, customer service, data validation, and quality assurance. Skilled at explaining complex processes, supporting team performance, troubleshooting technical issues, and improving workflows across multiple systems. Experienced in balancing accuracy with high-volume demands in deadline-driven environments. Currently expanding knowledge in business administration, medical coding, healthcare compliance, process improvement, and technology. Seeking remote evening or overnight opportunities with structured training and professional growth.",
