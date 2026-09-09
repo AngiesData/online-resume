3e9c119134db492e6873e78f64ef7e6d5c1cdeee
AngiesData <data@avidityexchange.com>
2026-09-08 21:57:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,9 +30,8 @@ const resumeData = {
     },
     {
       label: "Email",
-      value: "insurancewithangela@gmail.com",
-      link: "https://forms.gle/WTmX7LjEV7NKtMi27
-        "
+      value: "ttps://forms.gle/WTmX7LjEV7NKtMi27",
+      link: "https://forms.gle/WTmX7LjEV7NKtMi27"
     },
 /*    {
       label: "GitHub",
